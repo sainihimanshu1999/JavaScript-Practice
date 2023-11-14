8dbe24504b8287b887198a09dd6e708f5dca3a94
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-11-14 08:39:27 +0530

    basic prototype done

--- Extension/index.js
+++ Extension/index.js
@@ -2,13 +2,18 @@
 let myLeads = []
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
-let ulEl = document.getElementById('ul-el');
+let ulEl = document.getElementById('ul-el');  
 
 inputBtn.addEventListener('click', function() {
     let newLead = inputEl.value;
     myLeads.push(newLead)
-    console.log(myLeads)
-    for(let i=0; i<myLeads.length; i++){
-        console.log(myLeads[i])
-    }
+    renderLeads()
 })
+
+function renderLeads() {
+    sleads = ""
+    for(let i=0; i<myLeads.length; i++){
+        sleads += '<li>' + myLeads[i] + '</li>'
+    }
+    ulEl.innerHTML = sleads
+}
